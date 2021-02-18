af089395d6a76d63da0e1c2797830fa6b23176b6
Filip Matusak <filip.matusak@gmail.com>
2021-02-18 14:02:16 +0100

    Update environment name

--- dev-scripts/vnet/deploy.sh
+++ dev-scripts/vnet/deploy.sh
@@ -2,4 +2,4 @@
 
 docker build -t samo98/einvoice-slovenskosk .
 docker push samo98/einvoice-slovenskosk
-$JELASTIC_HOME/environment/control/redeploycontainers --envName einvoice-slovenskosk --nodeGroup cp --tag latest
+$JELASTIC_HOME/environment/control/redeploycontainers --envName dev-einvoice-upvs-mfsr --nodeGroup cp --tag latest
